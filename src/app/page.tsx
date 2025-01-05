24abb9501c66412a90bffd703befa2aca9e72d5d
Kader Bozkaya <87239102+kaderbozkaya@users.noreply.github.com>
2025-01-05 19:11:11 +0300

    home page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ import Hero from './components/hero'
 
 
  const  Homepage = async() => {
+  //sanityden tüm blog içeriklerini çekmek için:
      const query = `*[_type == 'blog']  | order(_updatedAt asc){
   Title,Paragraph,image,
    "slug":slug.current
-}`
+}` //sadece blog türündeki verileri alır.blogları güncelleme tarihine göre artan sırayla alır.title,paragraf,image slug.current ilgili alanları döndürür.slug alanını bit string olarak döndürür
 
 const data:Blog[] = await client.fetch(query)
 
